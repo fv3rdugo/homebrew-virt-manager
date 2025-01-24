ee01fb2cebebf6032f716fee940d80122e0c42c4
Fernando Verdugo <fernando.verdugo@upc.edu>
2025-01-24 08:44:34 +0100

    Add libosinfo
    libosinfo: The Operating System information database

---
+++ osinfo-db-tools.rb
@@ -0,0 +1,30 @@
+class OsinfoDbTools < Formula
+  desc "Tools for managing the libosinfo database files"
+  homepage "https://libosinfo.org"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-tools-1.12.0.tar.xz"
+  sha256 "f3315f675d18770f25dea8ed04b20b8fc80efb00f60c37ee5e815f9c3776e7f3"
+
+  depends_on "pkg-config" => :build
+
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "json-glib"
+  depends_on "libarchive" # need >= 3.0.0
+  depends_on "libsoup"
+
+  def install
+    args = %W[
+      --disable-dependency-tracking
+      --disable-silent-rules
+      --prefix=#{prefix}
+      --localstatedir=#{var}
+      --sysconfdir=#{etc}
+    ]
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/osinfo-db-path"
+  end
+end
---
+++ osinfo-db.rb
@@ -0,0 +1,19 @@
+class OsinfoDb < Formula
+  desc "Libosinfo database files"
+  homepage "https://libosinfo.org/"
+  url "https://releases.pagure.org/libosinfo/osinfo-db-20240701.tar.xz"
+  sha256 "e1d7381a72f0c45f473befa4a92fa010a37fc4f7b2bb5d1f68e06da440ef6905"
+
+  depends_on "osinfo-db-tools" => :build
+
+  def install
+    system "osinfo-db-import", "--local", cached_download
+
+    # Copy the archive into the prefix to avoid empty installation error
+    cp_r "./", prefix
+  end
+
+  test do
+    system "osinfo-db-validate", "--local"
+  end
+end
